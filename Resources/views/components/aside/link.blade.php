dfd283790f6a74b4f546793cca9f14cc994a96de
l3aro <dgbao1340@gmail.com>
2022-06-24 14:45:28 +0700

    fix target link sidebar

--- Resources/views/components/aside/link.blade.php
+++ Resources/views/components/aside/link.blade.php
@@ -2,6 +2,6 @@
 
 <!-- Current: "bg-green-800 text-white", Default: "text-green-100 hover:text-white hover:bg-green-600" -->
 <a {{ $attributes->class(['group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md', 'dark:bg-green-600 bg-green-800 text-white' => $active, 'text-green-100 hover:text-white hover:bg-green-600' => !$active]) }}
-    aria-current="page" target="{{ $newTab ? '_blank' : '_selft' }}">
+    aria-current="page" {{ $newTab ? 'target="_blank"' : '' }}>
     {{ $slot }}
 </a>
